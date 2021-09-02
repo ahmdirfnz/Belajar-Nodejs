c47319f87fdce12c7d855a311273d2ab5153ae68
irfanZ <46247388+irfanZ1357@users.noreply.github.com>
2021-09-02 19:56:10 +0800

    final commit for today

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const start = async () => {
         const first = await readFile('./content/first.txt', 'utf-8');
         const second = await readFile('./content/second.txt', 'utf-8');
         await writeFile(
-         './content/result-mind-grenade.txt',
+        './content/result-mind-grenade.txt',
         `This Is Awesome ${first} ${second}`,
         { flag: 'a' })
         console.log(first, second)
