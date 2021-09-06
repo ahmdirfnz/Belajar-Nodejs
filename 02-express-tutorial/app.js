c07c20778d8d048b613381c38c777cfcad327b6d
irfanZ <46247388+irfanZ1357@users.noreply.github.com>
2021-09-06 15:25:25 +0800

    Learn about router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,36 +1,22 @@
 const express = require('express')
-const app = express();
-const morgan = require('morgan')
-const port = 5000
-const logger = require('./logger')
-const authorize = require('./authorize')
-//  request => middleware => response
+const app = express()
 
-// 1. use vs route
-// 2. options - our own / express / third party
+const people = require('./router/people')
+const auth = require('./router/auth')
 
-// add route you want in front of logger to specify uses of logger function
-// app.use([logger, authorize])             
-// app.use(expres.static('./public'))  
-app.use(morgan('tiny'))
+// static assets
+app.use(express.static('./methods-public')) 
 
-app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1>')
-})
+// parse form data
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>')
-})
+//parse json
+app.use(express.json())
 
-app.get('/api/products', (req, res) => {
-    res.send('<h1>Products</h1>')
-})
+app.use('/api/people', people)
 
-app.get('/api/items', (req, res) => {
-    console.log(req.user);
-    res.send('<h1>Items</h1>')
-})
+app.use('/login', auth)
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000')
 })
